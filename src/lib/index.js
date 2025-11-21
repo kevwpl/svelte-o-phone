6ee2e9f22e8a815dd5e55570a0320582593a676b
Pierre <pierre-H@users.noreply.github.com>
2025-11-21 09:41:50 +0000

    Fix import

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,2 @@
-export { usePhonePicker } from './usePhonePicker.svelte.js';
+export { usePhonePicker } from './svelte-o-phone/usePhonePicker.svelte.js';
 export { countries } from './countries.js';
